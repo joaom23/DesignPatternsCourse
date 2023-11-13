d37672db5a901a6587bd58410c0a4df2b5a5893f
joaom23 <82104896+joaom23@users.noreply.github.com>
2023-11-13 18:26:37 +0000

    update

---
+++ Decorator/DynamicDecorator.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatternsCourse.Decorator;
+
+// Multiple decorators at run time.
+public class DynamicDecorator : IDemo
+{
+    public void Run()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Decorator/MultipleInheritanceWithInterfaces.cs
+++ Decorator/MultipleInheritanceWithInterfaces.cs
@@ -4,35 +4,65 @@ public class MultipleInheritanceWithInterfaces : IDemo
 {
     public void Run()
     {
-        
+        var d = new Dragon();
+        d.Crawl();
+        d.Fly();
+        d.Weight = 100;
     }
 
-    public class Bird
+    public interface IAnimal
     {
+        int Weight { get; set; }
+    }
+
+    public interface IBird : IAnimal
+    {
+        void Fly();
+    }
+
+    public class Bird : IBird
+    {
+        public int Weight { get; set; }
+
         public void Fly()
         {
             Console.WriteLine("Fly");
         }
     }
 
-    public class Lizard
+    public interface ILizard : IAnimal
     {
+        void Crawl();
+    }
+
+    public class Lizard : ILizard
+    {
+        public int Weight { get; set; }
+
         public void Crawl()
         {
             Console.WriteLine("Crawl");
         }
     }
 
-    public class Dragon 
+    public class Dragon : IBird, ILizard
     {
         private Bird bird;
         private Lizard lizard;
+        private int wheight;
+        public Dragon()
+        {
+            bird = new Bird();
+            lizard = new Lizard();
+        }
 
         public Dragon(Bird bird, Lizard lizard)
         {
             this.bird = bird ?? throw new ArgumentNullException(paramName: nameof(bird));
             this.lizard = lizard ?? throw new ArgumentNullException(paramName: nameof(lizard));
         }
+
+        public int Weight { get => wheight; set => wheight = value; }
 
         public void Crawl()
         {
