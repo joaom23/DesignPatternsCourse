3c96122858245ed5731284c9191cf2ac00551684
joaom23 <82104896+joaom23@users.noreply.github.com>
2023-11-16 17:37:07 +0000

    update

---
+++ Decorator/DecoratorExercice.cs
@@ -0,0 +1,73 @@
+﻿namespace DesignPatternsCourse.Decorator;
+
+public class DecoratorExercice : IDemo
+{
+    public void Run()
+    {
+        var d = new Dragon();
+        d.Age = 1;
+        Console.WriteLine(d.Crawl());
+        Console.WriteLine(d.Fly()); 
+        Console.WriteLine(d);
+    }
+
+    public class Bird
+    {
+        public int Age { get; set; }
+
+        public string Fly()
+        {
+            return (Age < 10) ? "flying" : "too old";
+        }
+    }
+
+    public class Lizard
+    {
+        public int Age { get; set; }
+
+        public string Crawl()
+        {
+            return (Age > 1) ? "crawling" : "too young";
+        }
+    }
+
+    public class Dragon // no need for interfaces
+    {
+        private Bird bird = new();
+        private Lizard lizard = new Lizard();
+
+        public Dragon()
+        {
+                
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set 
+            {
+                bird.Age = value;
+                lizard.Age = value;
+                age = value;
+            }
+        }
+
+
+        public string Fly()
+        {
+            return bird.Fly();
+        }
+
+        public string Crawl()
+        {
+            return lizard.Crawl();
+        }
+
+        public override string ToString()
+        {
+            return $"Age: {age}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,10 +8,12 @@ using DesignPatternsCourse.Builder.NormalBuilder;
 using DesignPatternsCourse.Builder.StepwiseBuilder;
 using DesignPatternsCourse.Composite;
 using DesignPatternsCourse.Decorator;
+using DesignPatternsCourse.Facade;
 using DesignPatternsCourse.Factories;
 using DesignPatternsCourse.Factories.AbstractFactory;
 using DesignPatternsCourse.Factories.BulkReplacement;
 using DesignPatternsCourse.Factories.PointExample;
+using DesignPatternsCourse.Flyweight;
 using DesignPatternsCourse.Prototype;
 using DesignPatternsCourse.Singleton;
 using System.Security.Cryptography;
@@ -20,6 +22,8 @@ class Program
 {
     public static void Main(string[] args)
     {
+        string showOptions = "opt";
+
         var options = new List<string>
         {
             "---Creational Patterns---\n",
@@ -29,36 +33,64 @@ class Program
             "3 - Stepwise builder",
             "4 - Functional builder",
             "5 - Builder exercice",
-            "-------------\n",
+            "-------------------------------------------------\n",
             "---Factories---",
             "6 - Point Example",
             "7 - Bulk Replacement",
             "8 - Abstract Factory",
             "9 - Factory exercice",
-            "-------------\n",
+            "-------------------------------------------------\n",
             "---Prototype---",
             "10 - Prototype Inheritance",
             "11 - Prototype Serializer",
-            "-------------\n",
+            "-------------------------------------------------\n",
             "---Singleton---",
             "12 - Singleton Exercice",
             "\n---Structural Patterns---\n",
             "---ADAPTER---",
             "13 - Adapter Exercice",
-            "\"-------------\n",
+            "-------------------------------------------------\n",
             "---BRIDGE---",
             "14 - Bridge example",
             "15 - Bridge exercice",
-            "-------------\n",
+            "-------------------------------------------------\n",
             "---COMPOSITE---",
             "16 - Composite Geometric Shapes",
             "17 - Composite Exercice",
-            "-------------\n",
+            "-------------------------------------------------\n",
             "---DECORATOR---",
             "18 - Multiple Inheritance With Interfaces",
-            "-------------\n",
+            "19 - Dynamic Decorator",
+            "20 - Decorator Exercice",
+            "-------------------------------------------------\n",
+            "---FACADE---",
+            "21 - Facade Exercice",
+            "-------------------------------------------------\n",
+            "---FLYWEIGHT---",
+            "22 - Text formatter example",
+            "23 - Flyweight Exercice",
+            "-------------------------------------------------\n",
+            "---PROXY---",
+            "24 - ",
+            "-------------------------------------------------\n",
             "0 - Exit"
         };
+
+        if (args != null && args.Length > 0)
+        {
+            if (args[0] == showOptions)
+            {
+                foreach (string option in options)
+                {
+                    Console.WriteLine(option);
+                }
+
+                return;
+            }
+
+            SelectClassToExecute(Convert.ToInt32(args[0]));
+            return;
+        }
 
         int keyNumber;
 
@@ -77,88 +109,7 @@ class Program
 
             if (parseSuccess)
             {
-                switch (keyNumber)
-                    {
-                        case 1:
-                            RunDemo(new BuilderDemo());
-                            break;
-
-                        case 2:
-                            RunDemo(new InheritanceBuilderDemo());
-                            break;
-
-                        case 3:
-                            RunDemo(new StepwiseBuilderDemo());
-                            break;
-
-                        case 4:
-                            RunDemo(new FunctinalBuilderDemo());
-                            break;
-
-                        case 5:
-                            RunDemo(new BuilderExercice());
-                            break;
-
-                        case 6:
-                            RunDemo(new PointeExampleDemo());
-                            break;
-
-                        case 7:
-                            RunDemo(new BulkReplacementDemo());
-                            break;
-
-                        case 8:
-                            RunDemo(new AbstractFactoryDemo());
-                            break;
-
-                        case 9:
-                            RunDemo(new FactoryExercice());
-                            break;
-
-                        case 10:
-                            RunDemo(new PrototypeInheritanceDemo());
-                            break;
-
-                        case 11:
-                            RunDemo(new PrototypeSerializerDemo());
-                            break;
-
-                        case 12:
-                            RunDemo(new SingletonExercice());
-                            break;
-
-                        case 13:
-                            RunDemo(new AdapterExercice());
-                            break;
-
-                        case 14:
-                            RunDemo(new BridgeExample());
-                            break;
-
-                        case 15:
-                            RunDemo(new BridgeExercice());
-                            break;
-
-                        case 16:
-                            RunDemo(new CompositeGeometricShapes());
-                            break;
-
-                        case 17:
-                            RunDemo(new CompositeExercice());
-                            break;
-
-                        case 18:
-                            RunDemo(new MultipleInheritanceWithInterfaces());
-                            break;
-
-                    case 0:
-                            break;
-
-                        default:
-                            Console.WriteLine("\nInvalid option. Press any key to continue...");
-                            Console.ReadLine();
-                            break;
-                }
+                SelectClassToExecute(keyNumber);
             }
             else
             {
@@ -167,6 +118,112 @@ class Program
                 keyNumber = -1;
             }
         } while (keyNumber != 0);
+    }
+
+    private static void SelectClassToExecute(int key)
+    {
+        switch (key)
+        {
+            case 1:
+                RunDemo(new BuilderDemo());
+                break;
+
+            case 2:
+                RunDemo(new InheritanceBuilderDemo());
+                break;
+
+            case 3:
+                RunDemo(new StepwiseBuilderDemo());
+                break;
+
+            case 4:
+                RunDemo(new FunctinalBuilderDemo());
+                break;
+
+            case 5:
+                RunDemo(new BuilderExercice());
+                break;
+
+            case 6:
+                RunDemo(new PointeExampleDemo());
+                break;
+
+            case 7:
+                RunDemo(new BulkReplacementDemo());
+                break;
+
+            case 8:
+                RunDemo(new AbstractFactoryDemo());
+                break;
+
+            case 9:
+                RunDemo(new FactoryExercice());
+                break;
+
+            case 10:
+                RunDemo(new PrototypeInheritanceDemo());
+                break;
+
+            case 11:
+                RunDemo(new PrototypeSerializerDemo());
+                break;
+
+            case 12:
+                RunDemo(new SingletonExercice());
+                break;
+
+            case 13:
+                RunDemo(new AdapterExercice());
+                break;
+
+            case 14:
+                RunDemo(new BridgeExample());
+                break;
+
+            case 15:
+                RunDemo(new BridgeExercice());
+                break;
+
+            case 16:
+                RunDemo(new CompositeGeometricShapes());
+                break;
+
+            case 17:
+                RunDemo(new CompositeExercice());
+                break;
+
+            case 18:
+                RunDemo(new MultipleInheritanceWithInterfaces());
+                break;
+
+            case 19:
+                RunDemo(new DynamicDecorator());
+                break;
+
+            case 20:
+                RunDemo(new DecoratorExercice());
+                break;
+
+            case 21:
+                RunDemo(new FacadeExercice());
+                break;
+
+            case 22:
+                RunDemo(new TextFormatterExample());
+                break;
+
+            case 23:
+                RunDemo(new FlyweightExercice());
+                break;
+
+            case 0:
+                break;
+
+            default:
+                Console.WriteLine("\nInvalid option. Press any key to continue...");
+                Console.ReadLine();
+                break;
+        }
     }
 
     private static void RunDemo(IDemo demo)
